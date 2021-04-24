62c07e90e333de48ba6a64ce700b169bf47a7c0e
xiaojay <xiaojay@gmail.com>
2021-04-24 23:46:53 +0800

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='grinmw',
-    version='0.1.0',
+    version='0.1.1',
     packages=['grinmw',],
     license='MIT',
     description = 'Python wrappers around the Grin wallet V3 and Grin node V2 APIs',
@@ -12,7 +12,7 @@ setuptools.setup(
     author_email = 'xiaojay@gmail.com, bladedoyle@gmail.com, marek.yggdrasil@gmail.com',
     install_requires=['requests', 'eciespy', 'coincurve', 'Crypto'],
     url = 'https://github.com/grinfans/grinmw.py',
-    download_url = 'https://github.com/grinfans/grinmw.py/archive/refs/tags/v0.1.0.tar.gz',
+    download_url = 'https://github.com/grinfans/grinmw.py/archive/refs/tags/v0.1.1.tar.gz',
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 2",
